173d3f128b4a18b1a6f303e114d508205c25d787
sameed baqai <sameedbaqai@sameeds-MacBook-Pro.local>
2025-05-01 22:51:52 -0400

    third push

--- backend/src/models/Users.ts
+++ backend/src/models/Users.ts
@@ -1,3 +1,28 @@
 export interface Users {
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+    authProvider: string;
+    createdAt?: Date;
+    lastLogin?: Date;
+    favoritePlayers: string[];
+    favoriteTeam: string;
+    role?: string;
+}
 
+export interface UserWriteDto {
+    name: string;
+    email: string;
+    password: string;
+    favoritePlayers?: string[];
+    favoriteTeam?: string;
+    role?: string;
+}
+
+export interface UserReadDto {
+    name: string;
+    email: string;
+    favoritePlayers?: string[];
+    favoriteTeam?: string;
 }
